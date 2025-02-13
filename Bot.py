94e8f099da9464ad9da94c49cd8fe04af99b138d
madhava04-44 <a4amr.04@gmail.com>
2025-02-13 13:51:32 +0530

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -95,3 +95,16 @@ def continuous_trading():
 import threading
 threading.Thread(target=continuous_trading).start()
 bot.polling()
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bot is Running!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
